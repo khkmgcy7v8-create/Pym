3740e2100dd909bcfa4f88309726ec4c16053b5e
khkmgcy7v8-create <khkmgcy7v8@privaterelay.appleid.com>
2026-09-08 11:35:13 -0400

    Register Pym disc renderer

---
+++ src/main/java/com/reirei/pym/client/ClientModEvents.java
@@ -0,0 +1,19 @@
+package com.reirei.pym.client;
+
+import com.reirei.pym.Pym;
+import com.reirei.pym.registry.ModEntities;
+import net.minecraft.client.renderer.entity.ThrownItemRenderer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Pym.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public final class ClientModEvents {
+    private ClientModEvents() {}
+
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(ModEntities.PYM_DISC.get(), ThrownItemRenderer::new);
+    }
+}
